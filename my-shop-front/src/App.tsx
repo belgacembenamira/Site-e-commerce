5c4c033909f34c41ffd6916cfe777a0ddb2b0061
belgacembenamira <benamierabelgacem@gmail.com>
2022-08-22 10:17:41 +0200

    feat: add `navigation` in app component

--- my-shop-front/src/App.tsx
+++ my-shop-front/src/App.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import FAQ from "./FAQ";
+import Contact from "./Contact";
+import Home from "./Home";
+import Form from "./Form";
+import Navbarh from "./Nav";
+import Footer from "./Footr";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <React.Fragment >
-      <h1>
-        hello
-      </h1>
-    </React.Fragment>
+    <BrowserRouter>
+      <Navbarh />
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/FAQ" element={<FAQ />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Form" element={<Form />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
